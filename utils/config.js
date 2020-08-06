37540051c551af282fb351474eeb1e693184abd5
Anna Lehto <anviinamaki@gmail.com>
2020-08-06 13:39:28 +0300

    Osa 4: teht. 4.8.

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,12 @@
+/* eslint-disable no-undef */
 require('dotenv').config()
 
 let PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
+
+if (process.env.NODE_ENV === 'test') {
+  MONGODB_URI = process.env.TEST_MONGODB_URI
+}
 
 module.exports = {
   PORT,
